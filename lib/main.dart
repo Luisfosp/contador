c7e2586fdd12a80029ab324dba94ea3448a48b2f
Luisfosp <luisfer2000tx@hotmail.com>
2023-05-24 18:41:10 -0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:contador/pages/home_page.dart';
+//import 'package:contador/pages/home_page.dart';
+import 'package:contador/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Material App',
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
       );
     
   }
